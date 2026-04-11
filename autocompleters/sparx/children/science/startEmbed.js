f2156316309af554fbd0b6009f470245cdbe86f9
Churros <gabiidiot76@gmail.com>
2026-04-11 22:06:34 +0100

    Images moved from imgur to postimage

--- autocompleters/sparx/children/science/startEmbed.js
+++ autocompleters/sparx/children/science/startEmbed.js
@@ -9,7 +9,7 @@ const section = new SectionBuilder()
 .addTextDisplayComponents(
     new TextDisplayBuilder().setContent(`# Sparx Science\nAutomate your Sparx Science homework with high accuracy and zero detection by your teacher. Simply press \`Login\` and follow the instructions to be on your way to acing your Sparx Science homework and never fearing of Sparx Science again in your life!\n## ✨ Features\n- ⏰ **Customisable Time** — Autocompletes your Sparx Science homework in whatever time you want to balance speed and detection!\n- 🎯 **High Accuracy** — Excellent accuracy of >90% at completing questions!\n- 🧠 **Easy To Use** — Simple and intuitive to use with the press of only a few buttons!`)
 )
-.setThumbnailAccessory(new ThumbnailBuilder({ media: { url: 'https://i.imgur.com/cGt5SC5.png' } }));
+.setThumbnailAccessory(new ThumbnailBuilder({ media: { url: 'https://i.postimg.cc/WpWDYDG2/c-Gt5SC5-Imgur.png' } }));
 // 2. Create the buttons and the action row
 const mathButtons = new ActionRowBuilder()
     .addComponents(
